8de46de375b925aeadf77c01376c91ccdc460a92
ju-lewis <boollewis04@gmail.com>
2025-01-29 11:34:00 +1100

    Update styling

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,16 @@
 
 const typstInput = document.getElementById("typst-input");
 
+let cvContent = "";
+let appContent = "";
+
 
 /**
  *  Changes the size of the `typstInput` textarea to match the number of newlines.
  *  This allows the box to grow dynamically.
  */
 function autoSizeInput() {
-    const numNewLines = [...typstInput.value.matchAll(/\n/g)].length + 2;
+    const numNewLines = Math.max([...typstInput.value.matchAll(/\n/g)].length + 2, 10);
 
     //TODO: Implement limit
 
@@ -15,4 +18,15 @@ function autoSizeInput() {
 
 }
 
+function handleInput(elem) {
 
+    // Get input content
+    if (elem.id === "cv-content") {
+        cvContent = elem.value;
+    } else if (elem.id === "app-content") {
+        appContent = elem.value;
+    } else return;
+
+    // Replace displayed content in textbox with loading symbol
+
+}
