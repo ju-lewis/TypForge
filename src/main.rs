5cf7ee2e22dbc94367f5e0f1b918fbe2b9451740
ju-lewis <julianlewis2023@gmail.com>
2025-01-29 18:13:45 +1100

    Implement template generation route

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,45 @@
 
 
 use tokio;
-use axum::{self, http::StatusCode, response::Html, routing::{get, post}, Router};
+use axum::{self, extract::{State, Json}, http::StatusCode, response::Html, routing::{get, post}, Router};
 use tower_http::services::ServeDir;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use reqwest::Client;
+
+
+
+const GEMINI_API_ENDPOINT: &'static str  = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$";
+const API_KEY: &'static str = env!("GEMINI_API_KEY");
+
+
+#[derive(Clone)]
+struct AppState {
+    client: Client
+}
+
+#[derive(Deserialize, Serialize)]
+struct TemplateRequest {
+    cv: String,
+    spec: String
+}
 
 
 
 #[tokio::main]
 async fn main() {
 
+    let state = AppState {
+        client: Client::new()
+    };
+
     // Define app routes
     let app = Router::new()
         .route("/", get(read_index))
         .route("/template", post(create_template))
         .route("/rendered-pdf", get(render_pdf))
-        .fallback_service(ServeDir::new("frontend"));
+        .fallback_service(ServeDir::new("frontend"))
+        .with_state(state);
 
 
     // Create TCP listener and serve app
@@ -47,8 +72,44 @@ async fn read_index() -> Result<Html<String>, StatusCode> {
 }
 
 /// Query the Google Gemini?? API to generate an application-specific Typst cover letter template.
-async fn create_template() {
-    todo!();
+async fn create_template(State(state): State<AppState>, Json(r): Json<TemplateRequest>) -> Result<String, StatusCode> {
+
+    let url = String::from(GEMINI_API_ENDPOINT) + API_KEY;
+    
+    // Create full prompt
+    let prompt = match std::fs::read_to_string("prompt.txt") {
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+        Ok(p) => p + &format!("\nCV Content: {}\n\nJob/application listing content: {}", r.cv, r.spec)
+    };
+
+
+    let res = state.client.post(url)
+        .header("Content-Type", "application/json")
+        .body(json!({
+            "contents": [{
+                "parts": [{"text": prompt}]
+            }]
+        }).to_string())
+        .send().await;
+    
+    if res.is_err() {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR)
+    }
+
+    let json: Value = match res.unwrap().json().await {
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+        Ok(j) => j
+    };
+
+    // Pull text from API response
+    let content = match json["candidates"][0]["content"].as_str() {
+        None => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+        Some(c) => c
+    }.to_string();
+
+    
+
+    Ok(content)
 }
 
 /// Render the raw Typst code into a PDF
